62536d9557d09f1fb3b31bbd507d21ee2f1b3f30
GustavP22R <gustavservilieri@gmail.com>
2024-04-26 13:45:03 +0200

    Ver 1.01
    Minimal ting, grund ideer indebæret

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() 
+{
+  createCanvas(windowWidth, windowHeight);
+
+  console.log(windowHeight);
+  console.log(windowWidth);
+}
+
+function draw() 
+{
+  background(165,212,112);
+
+
+  push();
+  strokeWeight(10);
+  stroke(168, 68,50);
+  line(400,400,600,600);
+  pop();
+
+
+  strokeWeight(2);
+  circle(350,400,30);
+}
