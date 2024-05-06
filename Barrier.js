d34489e3d6450ae5268619e1d9e27b77270d9d76
GustavP22R <gustavservilieri@gmail.com>
2024-05-06 14:35:55 +0200

    Ver 1.03
    Lavet koden, kan ikke se nogle barriers

--- Barrier.js
+++ Barrier.js
@@ -1,11 +1,51 @@
 class Barrier
 {
-    constructor(mouseX, mouseY)
+    constructor(mouseX, mouseY, windowHeight)
     {
+        this.barrierLength = windowHeight / 4;
+
+        this.mouseXPosition = mouseX;   
+        this.mouseYPosition = mouseY;
+    }
+
+    createBarrier()
+    {
+        push();
+        strokeWeight(10);
+        stroke(168, 68,50);
+        line(this.xStart,this.yStart,this.xSlut,this.ySlut);
+        pop();
+
+        if(mouseIsPressed)
+        {
+            this.pressCount += 1;
+        }
+
+        if(this.pressCount == 0)
+        {
+            this.xStart = this.mouseXPosition;
+            this.yStart = this.mouseYPosition;
+            
+            this.xSlut = this.mouseXPosition;
+            this.ySlut = this.mouseYPosition;
+        }
+
+        if(this.pressCount == 1)
+        {
+            this.xStart = this.mouseXPosition;
+            this.yStart = this.mouseYPosition;
+        }
+
+        if(this.pressCount == 2)
+        {
+            this.xSlut = this.mouseXPosition;
+            this.ySlut = this.mouseYPosition;
+
+        }
     }
 
     destroyBarrier()
     {
-        
+
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,22 +1,79 @@
+
+
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
 
   console.log(windowHeight);
   console.log(windowWidth);
+
+  barrier=new Barrier();
+
+
+
 }
 
 function draw() 
 {
   background(165,212,112);
-  
-  push();
-  strokeWeight(10);
-  stroke(168, 68,50);
-  line(400,400,600,600);
-  pop();
 
+  strokeWeight(5);
+  line(200,200,400,400);
 
   strokeWeight(2);
   circle(350,400,30);
+  
+barrier.createBarrier();
+}
+
+class Barrier
+{
+    constructor(mouseX, mouseY, windowHeight)
+    {
+        this.barrierLength = windowHeight / 4;
+
+        this.mouseXPosition = mouseX;   
+        this.mouseYPosition = mouseY;
+    }
+
+    createBarrier()
+    {
+        push();
+        strokeWeight(10);
+        stroke(168, 68,50);
+        line(this.xStart,this.yStart,this.xSlut,this.ySlut);
+        pop();
+
+        if(mouseIsPressed)
+        {
+            this.pressCount += 1;
+        }
+
+        if(this.pressCount == 0)
+        {
+            this.xStart = this.mouseXPosition;
+            this.yStart = this.mouseYPosition;
+            
+            this.xSlut = this.mouseXPosition;
+            this.ySlut = this.mouseYPosition;
+        }
+
+        if(this.pressCount == 1)
+        {
+            this.xStart = this.mouseXPosition;
+            this.yStart = this.mouseYPosition;
+        }
+
+        if(this.pressCount == 2)
+        {
+            this.xSlut = this.mouseXPosition;
+            this.ySlut = this.mouseYPosition;
+
+        }
+    }
+
+    destroyBarrier()
+    {
+
+    }
 }
