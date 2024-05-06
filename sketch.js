1ee25ba78cba52ec14914b5029f33200206d530d
GustavP22R <gustavservilieri@gmail.com>
2024-05-06 15:10:51 +0200

    Ver 1.04
    Man kan lave murer, de forsvinder ikke efter 2 sekunder, klik flere gange for at de forsvinder

--- sketch.js
+++ sketch.js
@@ -1,79 +1,53 @@
+xStart = 0;
+yStart = 0;
 
+xEnd = 0;
+yEnd = 0;
+
+pressCount = 0;
 
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
-
-  console.log(windowHeight);
-  console.log(windowWidth);
-
-  barrier=new Barrier();
-
-
-
 }
 
 function draw() 
 {
-  background(165,212,112);
+ background(165,212,112);
 
-  strokeWeight(5);
-  line(200,200,400,400);
+ strokeWeight(5);
+ line(200,200,400,400);
 
-  strokeWeight(2);
-  circle(350,400,30);
-  
-barrier.createBarrier();
+ strokeWeight(2);
+ circle(350,400,30);
+
+// Make a barrier
+if(pressCount == 0)
+{
+  xStart = mouseX;
+  yStart = mouseY;
+
+  xEnd = NaN;
+  yEnd = NaN;  
 }
 
-class Barrier
+if(pressCount == 1)
 {
-    constructor(mouseX, mouseY, windowHeight)
-    {
-        this.barrierLength = windowHeight / 4;
+  xEnd = mouseX;
+  yEnd = mouseY;  
+}
 
-        this.mouseXPosition = mouseX;   
-        this.mouseYPosition = mouseY;
-    }
+point(xStart,yStart);
 
-    createBarrier()
-    {
-        push();
-        strokeWeight(10);
-        stroke(168, 68,50);
-        line(this.xStart,this.yStart,this.xSlut,this.ySlut);
-        pop();
+if(pressCount == 3)
+{
+  pressCount = 0;
+}  
+    
+  line(xStart,yStart,xEnd,yEnd);
+}
 
-        if(mouseIsPressed)
-        {
-            this.pressCount += 1;
-        }
-
-        if(this.pressCount == 0)
-        {
-            this.xStart = this.mouseXPosition;
-            this.yStart = this.mouseYPosition;
-            
-            this.xSlut = this.mouseXPosition;
-            this.ySlut = this.mouseYPosition;
-        }
-
-        if(this.pressCount == 1)
-        {
-            this.xStart = this.mouseXPosition;
-            this.yStart = this.mouseYPosition;
-        }
-
-        if(this.pressCount == 2)
-        {
-            this.xSlut = this.mouseXPosition;
-            this.ySlut = this.mouseYPosition;
-
-        }
-    }
-
-    destroyBarrier()
-    {
-
-    }
+function mousePressed()
+{
+  pressCount = pressCount + 1;
 }
