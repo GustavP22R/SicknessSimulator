54875e376f020d9f3384b0abd11121234b5a6b59
GustavP22R <gustavservilieri@gmail.com>
2024-05-14 14:22:58 +0200

    Ver 1.05
    Barrier har defineret længde og forsvinder efter 2 sekunder, ikke noget collision detection indtil videre

--- sketch.js
+++ sketch.js
@@ -4,50 +4,91 @@ yStart = 0;
 xEnd = 0;
 yEnd = 0;
 
-pressCount = 0;
+pressCount = 1;
+
+let timer;
 
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
+
+  barrierLimit = 0.15625 * windowWidth;
+
+  console.log(windowWidth);
 }
 
 function draw() 
 {
- background(165,212,112);
+    background(165,212,112);
 
- strokeWeight(5);
- line(200,200,400,400);
+    strokeWeight(5);
+    line(200,200,500,200);
 
- strokeWeight(2);
- circle(350,400,30);
+    strokeWeight(2);
+    circle(350,400,30);
 
-// Make a barrier
-if(pressCount == 0)
-{
-  xStart = mouseX;
-  yStart = mouseY;
-
-  xEnd = NaN;
-  yEnd = NaN;  
-}
-
-if(pressCount == 1)
-{
-  xEnd = mouseX;
-  yEnd = mouseY;  
-}
-
-point(xStart,yStart);
-
-if(pressCount == 3)
-{
-  pressCount = 0;
-}  
     
-  line(xStart,yStart,xEnd,yEnd);
+  // Make a barrier
+  if (pressCount == 1) {
+    xStart = mouseX;
+    yStart = mouseY;
+
+    xEnd = NaN;
+    yEnd = NaN;
+  } 
+    
+  else if (pressCount == 2) 
+    {
+    let lengthX = mouseX - xStart;
+    let lengthY = mouseY - yStart;
+    let barrierLength = sqrt(sq(lengthX) + sq(lengthY));
+
+    if (barrierLength >= barrierLimit) 
+    {
+      let angle = atan2(lengthY, lengthX);
+      xEnd = xStart + cos(angle) * barrierLimit;
+      yEnd = yStart + sin(angle) * barrierLimit;
+    } 
+    else 
+    {
+      xEnd = mouseX;
+      yEnd = mouseY;
+    }
+
+    // Start the timer to clear the line after 2 seconds
+    if (timer) 
+    {
+      clearTimeout(timer);
+    }
+    //Second perimeter is time in ms
+    timer = setTimeout(clearBarrier, 2000);
+  }
+
+  if (pressCount > 3) 
+  {
+    pressCount = 1;
+  }
+
+ 
+    strokeWeight(5);
+    point(xStart, yStart);
+    line(xStart, yStart, xEnd, yEnd);
+    
+  
 }
 
-function mousePressed()
+function mousePressed() 
 {
-  pressCount = pressCount + 1;
+  pressCount++;
+}
+
+// Function to clear the barrier
+function clearBarrier() 
+{
+  pressCount = 1;
+  xStart = 0;
+  yStart = 0;
+  xEnd = 0;
+  yEnd = 0;
+  timer = null;
 }
