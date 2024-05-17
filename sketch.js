34f24acdf916ab8b263b293af430286303bf2f61
GustavP22R <gustavservilieri@gmail.com>
2024-05-17 13:22:06 +0200

    Ver 1.06
    Barrier er nu en class og vil kunne intereagere med andre classes

--- sketch.js
+++ sketch.js
@@ -1,20 +1,12 @@
-xStart = 0;
-yStart = 0;
-
-xEnd = 0;
-yEnd = 0;
-
-pressCount = 1;
-
-let timer;
+let pressCount = 1;
+let barrier;
 
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
-
-  barrierLimit = 0.15625 * windowWidth;
-
   console.log(windowWidth);
+
+  barrier = new Barrier();
 }
 
 function draw() 
@@ -27,68 +19,124 @@ function draw()
     strokeWeight(2);
     circle(350,400,30);
 
+
+    barrier.updateBarrier();
+    barrier.display();
+
     
-  // Make a barrier
-  if (pressCount == 1) {
-    xStart = mouseX;
-    yStart = mouseY;
-
-    xEnd = NaN;
-    yEnd = NaN;
-  } 
-    
-  else if (pressCount == 2) 
-    {
-    let lengthX = mouseX - xStart;
-    let lengthY = mouseY - yStart;
-    let barrierLength = sqrt(sq(lengthX) + sq(lengthY));
-
-    if (barrierLength >= barrierLimit) 
-    {
-      let angle = atan2(lengthY, lengthX);
-      xEnd = xStart + cos(angle) * barrierLimit;
-      yEnd = yStart + sin(angle) * barrierLimit;
-    } 
-    else 
-    {
-      xEnd = mouseX;
-      yEnd = mouseY;
-    }
-
-    // Start the timer to clear the line after 2 seconds
-    if (timer) 
-    {
-      clearTimeout(timer);
-    }
-    //Second perimeter is time in ms
-    timer = setTimeout(clearBarrier, 2000);
-  }
-
-  if (pressCount > 3) 
-  {
-    pressCount = 1;
-  }
-
- 
-    strokeWeight(5);
-    point(xStart, yStart);
-    line(xStart, yStart, xEnd, yEnd);
-    
-  
 }
 
 function mousePressed() 
 {
-  pressCount++;
+ 
+  if (pressCount < 3) 
+  {
+    pressCount++;
+  }
+  
 }
 
-// Function to clear the barrier
-function clearBarrier() 
+class Barrier
 {
-  pressCount = 1;
-  xStart = 0;
-  yStart = 0;
-  xEnd = 0;
-  yEnd = 0;
-  timer = null;
+   constructor()
+   {
+    this.xStart = 0;
+    this.yStart = 0;
+    this.xEnd = 0;
+    this.yEnd = 0;
+    this.barrierLimit = 0.15625 * windowWidth;
+
+    this.timer = null;
+
+    this.barrierColorR = 0;
+    this.barrierColorG = 0;
+    this.barrierColorB = 0;
+   }
+
+   updateBarrier() 
+   {
+      if (pressCount === 1) 
+      {
+         this.xStart = mouseX;
+         this.yStart = mouseY;
+
+         this.xEnd = NaN;
+         this.yEnd = NaN;
+
+         this.barrierColorR = 0;
+         this.barrierColorG = 0;
+         this.barrierColorB = 0;
+      } 
+
+      else if (pressCount === 2) 
+      {
+         let lengthX = mouseX - this.xStart;
+         let lengthY = mouseY - this.yStart;
+         let barrierLength = sqrt(sq(lengthX) + sq(lengthY));
+
+         this.barrierColorR = 0;
+         this.barrierColorG = 0;
+         this.barrierColorB = 0;
+
+        if (barrierLength >= this.barrierLimit) 
+        {
+          let angle = atan2(lengthY, lengthX);
+          this.xEnd = this.xStart + cos(angle) * this.barrierLimit;
+          this.yEnd = this.yStart + sin(angle) * this.barrierLimit;
+        } 
+
+        else 
+        {
+          this.xEnd = mouseX;
+          this.yEnd = mouseY;
+        }
+
+        
+          // Start the timer to clear the line after 2 seconds
+          if (this.timer) 
+          {
+            clearTimeout(this.timer);
+          }
+
+          this.timer = setTimeout(() => this.clearBarrier(), 2000);
+        
+      }
+   }
+
+
+   display() 
+   {
+      if (pressCount === 1 || pressCount === 2 || pressCount === 3) 
+      {
+        // Draws barrier
+        strokeWeight(5);
+        point(this.xStart, this.yStart);
+
+        if (!isNaN(this.xEnd) && !isNaN(this.yEnd)) 
+        {
+          push();
+          if(pressCount === 3)
+          {
+            this.barrierColorR = 255;
+            this.barrierColorG = 0;
+            this.barrierColorB = 0;
+          }
+          
+          stroke(this.barrierColorR, this.barrierColorG, this.barrierColorB);
+          line(this.xStart, this.yStart, this.xEnd, this.yEnd);
+          pop();
+        }
+      }
+   }
+
+   clearBarrier() 
+   {
+      pressCount = 1;
+      this.xStart = 0;
+      this.yStart = 0;
+      this.xEnd = 0;
+      this.yEnd = 0;
+      this.timer = null;
+   }
 }
+
