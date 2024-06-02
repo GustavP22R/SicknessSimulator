32e002af89439d777fcddbe928b39dcf8be9506f
GustavP22R <48871297+GustavP22R@users.noreply.github.com>
2024-06-02 21:29:28 +0200

    VERSION BETA
    Alfa fejlede, rigtig version uploade nu nu skal det afleveres

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+//Global variables 
+
 let pressCount = 1;
 let barrier;
 
@@ -8,10 +10,13 @@ let house=[];
 
 let human = [];
 let chance = 30;
-let startSick=5;
+let startSick = 5;
 
 let sick = 0;
 
+let numberHouses = 2;
+
+//Loads image of house
 function preload()
 {
   houseImage = loadImage('house.png');
@@ -20,21 +25,23 @@ function preload()
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
-  console.log(windowWidth);
 
-  for(let i=0;i<5;i++)
+  //Creates houses
+  for(let i = 0; i < numberHouses; i++)
   { 
-    house[i]=new House();
+    house[i] = new House();
 
     for(let j = 0; j < house.length; j++)
     {
-     if(i!=j && house[i].check(house[j])==true)
-      {
-       house[i].relocate();
-      }
+      //Moves house again if houses to close
+      if(i!=j && house[i].check(house[j])==true)
+        {
+        house[i].relocate();
+        }
     }   
- }
+  }
 
+  //Creates humans
   for (let k = 0; k < 15; k++) 
   {
     human[k] = new Human(chance,startSick,k);
@@ -44,19 +51,12 @@ function setup()
        if (k != l && human[k].intersectHuman(human[l])) 
        {
           human[k].check(human[l]);
-        }
+       }
      }  
+  }
 
-}
-
+  //Barrier
   barrier = new Barrier();
-
-  //Choose number of houses - More than 20 houses and it crashes
-  // houseGenerator = new HouseGenerator(5);
-  // houseGenerator.GenerateHouses();
-
-
-
 
 }
 
@@ -64,12 +64,11 @@ function draw()
 { 
     background(109, 210, 247);
 
+    //Background of map
     push();
     fill(165,212,112);
     rect(20,20, width - 40, height - 40, 20);
     pop();
-
-
 
     strokeWeight(2);
 
@@ -78,44 +77,46 @@ function draw()
     barrier.DisplayBarrier();
 
     //draws people
-  for (let i = 0; i < human.length; i++) {
-    human[i].show();
-    human[i].checkBarrierCollision(barrier);
+    for (let i = 0; i < human.length; i++) 
+    {
+      human[i].show();
+      human[i].checkBarrierCollision(barrier);
     
-    for (let j = 0; j < human.length; j++) {    
+      for (let j = 0; j < human.length; j++) 
+      {    
       
-      if (i != j && human[i].intersectHuman(human[j])) {
-        human[i].infect(human[j]);
-        //    human[i].collide(human[j]);
-        human[i].bounce();
-      }      
-        for(let k=0;k<house.length;k++)
-  {
-house[k].show();
+        //Humans touch eachother
+        if (i != j && human[i].intersectHuman(human[j])) 
+        {
+          //Infect human
+          human[i].infect(human[j]);
 
-if(human[i].intersectHouse(house[k]))
-{
-human[i].enterHouse(house[k]);
+          //Give human new direction
+          human[i].bounce();
+        }      
+        
+        for(let k = 0; k < house.length; k++)
+        {
+          //Show image on house
+          house[k].show();
 
-}
+            //If human and house touch
+            if(human[i].intersectHouse(house[k]))
+            {
+            human[i].enterHouse(house[k]);
+            }
+         }
+      } 
+     }
 
-  }
-    } 
-
-  }
-
-
-    //Houses display
-  // houseGenerator.DisplayHouses(); 
-
+  //text on how man are infected and number of humans   
   push();
   textSize(20);
   strokeWeight(1);
   stroke(0);
   text("Amount of people: " + human.length,45,60);
   text("Amount of infected: " + sick,45,110);
-      pop();
-
+  pop();
 }
 
 
